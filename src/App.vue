d3887fa9137fe721b3856b74a73fa25583fd8a45
Madyan Eka <madyan.eka95@gmail.com>
2018-10-16 14:22:16 +0700

    switching multiple components with dynamic components

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,18 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <app-quote>
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <component :is="selectedComponent">
+                    <p>Default Content</p>
+                </component>
+                <!-- <app-quote>
                     <h2 slot="title">{{ quoteTitle }}</h2>
                     <p slot="content">A wonderful quote!</p>
-                </app-quote>
+                </app-quote> -->
             </div>
         </div>
     </div>
@@ -13,14 +21,19 @@
 
 <script>
     import Quote from './components/Quote.vue'
+    import Author from './components/Author.vue'
+    import New from './components/New.vue'
 
     export default {
         data: () => ({
-            quoteTitle: 'The Quote'
+            quoteTitle: 'The Quote',
+            selectedComponent: 'appQuote'
         }),
 
         components: {
-            appQuote: Quote
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
         }
     }
 </script>
