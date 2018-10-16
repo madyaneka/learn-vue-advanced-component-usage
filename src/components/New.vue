edef53f6215d015e6819b06ee2cd14074718b062
Madyan Eka <madyan.eka95@gmail.com>
2018-10-16 14:31:14 +0700

    dynamic component lifecycle hooks

--- src/components/New.vue
+++ src/components/New.vue
@@ -16,6 +16,14 @@ export default {
 
   destroyed() {
     console.log('Destroyed!')
+  },
+
+  deactivated() {
+    console.log('Deactivated!')
+  },
+
+  activated() {
+    console.log('Activated!')
   }
 }
 </script>
